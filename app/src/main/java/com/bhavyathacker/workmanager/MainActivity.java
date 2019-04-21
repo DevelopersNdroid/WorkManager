e49df9821fb299356c52d3aa6021491f33282391
bhavyathacker <developersndroid@gmail.com>
2019-04-22 00:02:14 +0530

    Add Constraints

--- app/src/main/java/com/bhavyathacker/workmanager/MainActivity.java
+++ app/src/main/java/com/bhavyathacker/workmanager/MainActivity.java
@@ -4,17 +4,22 @@ import android.arch.lifecycle.Observer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.work.Constraints;
 import androidx.work.Data;
+import androidx.work.NetworkType;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
     public static final String KEY_TASK_DESC = "key_task_desc";
+    OneTimeWorkRequest workRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +28,18 @@ public class MainActivity extends AppCompatActivity {
         Data data = new Data.Builder()
                 .putString(KEY_TASK_DESC, "Hey I am sending the work data")
                 .build();
-        final OneTimeWorkRequest workRequest = new OneTimeWorkRequest.Builder(MyWorker.class)
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.METERED)
+                .build();
+        workRequest = new OneTimeWorkRequest.Builder(MyWorker.class)
+                .setConstraints(constraints)
                 .setInputData(data)
                 .build();
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                WorkManager.getInstance().enqueue(workRequest);
+//                WorkManager.getInstance().enqueue(workRequest);
+                scheduleWork();
 
             }
         });
@@ -37,18 +47,36 @@ public class MainActivity extends AppCompatActivity {
         WorkManager.getInstance().getWorkInfoByIdLiveData(workRequest.getId()).observe(this, new Observer<WorkInfo>() {
             @Override
             public void onChanged(@Nullable WorkInfo workInfo) {
+                Log.d(TAG, "onChanged: ");
                 if (workInfo != null) {
+                    Log.d(TAG, "onChanged: workInfo != null");
                     if (workInfo.getState().isFinished()) {
+                        Log.d(TAG, "onChanged: Finished");
                         Data data = workInfo.getOutputData();
                         String output = data.getString(MyWorker.KEY_TASK_OUTPUT);
                         textView.append(output + "\n");
                     }
                     String status = workInfo.getState().name();
                     textView.append(status + "\n");
+                    Log.d(TAG, "onChanged: status: " + workInfo.getState().name());
+//                    scheduleWork();
                 }
 
 
             }
         });
     }
+
+    /*private void cancelWork() {
+        Log.d(TAG, "cancelWork: ");
+        WorkManager.getInstance().cancelWorkById(workRequest.getId());
+        scheduleWork();
+    }*/
+
+    private void scheduleWork() {
+        Log.d(TAG, "scheduleWork: ");
+        WorkManager.getInstance().enqueue(workRequest);
+    }
+
+
 }
--- app/src/main/java/com/bhavyathacker/workmanager/MyWorker.java
+++ app/src/main/java/com/bhavyathacker/workmanager/MyWorker.java
@@ -5,12 +5,15 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import androidx.work.Data;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 public class MyWorker extends Worker {
+
+    public static final String TAG = MyWorker.class.getSimpleName();
 
     public static final String KEY_TASK_OUTPUT = "key_task_output";
 
@@ -40,7 +43,18 @@ public class MyWorker extends Worker {
     public Result doWork() {
         Data data = getInputData();
         String desc = data.getString(MainActivity.KEY_TASK_DESC);
-        displayNotification("My Worker", desc);
+//        displayNotification("My Worker", desc);
+        Log.d(TAG, "doWork: " + desc);
+        for (int i = 0; i < 25; i++) {
+            Log.d(TAG, "doWork: " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+//        displayNotification("Work", "Task finished successfully..!!");
+
         Data data1 = new Data.Builder()
                 .putString(KEY_TASK_OUTPUT, "Task finished successfully..!!")
                 .build();
@@ -51,10 +65,10 @@ public class MyWorker extends Worker {
     private void displayNotification(String title, String task) {
         NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel("simplifiedcoding", "simplifiedcoding", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationChannel channel = new NotificationChannel("worknotification", "worknotification", NotificationManager.IMPORTANCE_DEFAULT);
             notificationManager.createNotificationChannel(channel);
         }
-        NotificationCompat.Builder notification = new NotificationCompat.Builder(getApplicationContext(), "simplifiedcoding")
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(getApplicationContext(), "worknotification")
                 .setContentTitle(title)
                 .setContentText(task)
                 .setSmallIcon(R.mipmap.ic_launcher);
